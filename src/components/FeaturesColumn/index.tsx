aa74b8d12d68565003087561f178e2035f4b71b5
hkclki <hokheil26@gmail.com>
2023-12-31 16:45:03 -0500

    [REFACTOR] udpate styles

--- src/components/FeaturesColumn/index.tsx
+++ src/components/FeaturesColumn/index.tsx
@@ -26,49 +26,49 @@ export default function FeaturesColumn() {
     {
       title: "Breathing Exercises",
       backgroundColor: "bg-yellow-200",
-      icon: <Wind className="h-12 w-12" />,
+      icon: <Wind className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/breathing",
     },
     {
       title: "Daily Check-In",
       backgroundColor: "bg-amber-600",
-      icon: <CheckCircle className="h-12 w-12" />,
+      icon: <CheckCircle className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
     {
       title: "Cultivating Black Joy",
       backgroundColor: "bg-pink-500",
-      icon: <Smile className="h-12 w-12" />,
+      icon: <Smile className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
     {
       title: "Guide to Sleep",
       backgroundColor: "bg-lime-300",
-      icon: <BedDouble className="h-12 w-12" />,
+      icon: <BedDouble className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
     {
       title: "Beginning Meditation",
       backgroundColor: "bg-purple-400",
-      icon: <Brain className="h-12 w-12" />,
+      icon: <Brain className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
     {
       title: "The Shine Collection",
       backgroundColor: "bg-teal-300",
-      icon: <Heart className="h-12 w-12" />,
+      icon: <Heart className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
     {
       title: "Women's Collection",
       backgroundColor: "bg-sky-400",
-      icon: <Ear className="h-12 w-12" />,
+      icon: <Ear className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
     {
       title: "Parents and Kids",
       backgroundColor: "bg-violet-500",
-      icon: <Slice className="h-12 w-12" />,
+      icon: <Slice className="h-6 w-6 md:h-12 md:w-12" />,
       href: "/",
     },
   ];
@@ -78,9 +78,11 @@ export default function FeaturesColumn() {
       <div className="grid grid-cols-2 gap-4">
         {features.map((feature, index) => (
           <Link href={feature.href} key={index}>
-            <Card className={`w-full ${feature.backgroundColor}`}>
+            <Card className={`h-full  w-full ${feature.backgroundColor}`}>
               <CardHeader>
-                <CardTitle>{feature.title}</CardTitle>
+                <CardTitle className="text-md md:text-lg">
+                  {feature.title}
+                </CardTitle>
               </CardHeader>
               <CardContent>{feature.icon}</CardContent>
             </Card>
