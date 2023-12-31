152ccd813cdb0b6d3484b7266d4ae8797ab69d18
hkclki <hokheil26@gmail.com>
2023-12-31 15:48:09 -0500

    [FEATURE] googleProvider, display icon

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import { LogoutButton } from "~/components/auth/logout";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { getServerAuthSession } from "~/server/auth";
 
 async function AccountInfo() {
@@ -11,7 +12,19 @@ async function AccountInfo() {
 
   return (
     <div className="flex px-6 pt-8">
-      <LogoutButton />
+      <div className="flex-1">
+        <h1 className="text-2xl font-bold">Account</h1>
+        {auth.user && (
+          <div>
+            <Avatar className="my-4 h-24 w-24">
+              <AvatarImage src={auth.user?.image ?? ""} />
+              <AvatarFallback></AvatarFallback>
+            </Avatar>
+            <p className="text-black-500">{auth.user.email}</p>
+          </div>
+        )}
+        <LogoutButton />
+      </div>
     </div>
   );
 }
